8ac0ad1dbf81b3ea1361da0e778d4a223562104d
Maxwell <maxwell@Maxwell.max>
2024-02-05 23:03:58 -0300

    scroll suave

--- js/app.js
+++ js/app.js
@@ -23,3 +23,24 @@ btnMobile.addEventListener('touchstart', toggleMenu);
 linkMenu.addEventListener('click', toggleMenu);
 linkMenu.addEventListener('touchstart', toggleMenu);
 
+// scroll suave
+
+const menuItens = document.querySelectorAll('#menu a');
+
+menuItens.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const element = event.target;
+  const id = element.getAttribute('href')
+  const to = document.querySelector(id).offsetTop;
+
+
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+
+}
